3a613296b1a823eddaa21fd5c3632cd023685940
siddheshghare <siddheshghare6112@gmail.com>
2025-01-16 12:34:05 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Banner from './components/Banner'
+import Footer from './components/footer'
+import FreeBook from './components/FreeBook'
+
+function App() {
+  return (
+    <div>
+     <Navbar/>
+     <Banner/>
+     <FreeBook/>
+     <Footer/>
+
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/FreeBook.jsx
@@ -0,0 +1,94 @@
+import React from 'react'
+import list from "../../public/list.json"
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+function FreeBook() {
+
+
+    const filteredData = list.filter((data) => data.category === "free")
+    console.log(filteredData);
+
+
+    var settings = {
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 4,
+        initialSlide: 0,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    };
+
+
+
+    return (
+        <>
+            <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4 ">
+                <div>
+                    <h1 className="font-bold text-xl">Free Offered Courses</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas deleniti assumenda impedit iste, numquam suscipit nisi quia amet dolorum fuga at? Officiis temporibus in corrupti cum recusandae. Amet harum nostrum maiores ducimus? Reprehenderit, expedita. Quis!</p>
+                </div>
+
+
+                <div>
+                    <Slider {...settings}>
+                        <div>
+                            <h3>1</h3>
+                        </div>
+                        <div>
+                            <h3>2</h3>
+                        </div>
+                        <div>
+                            <h3>3</h3>
+                        </div>
+                        <div>
+                            <h3>4</h3>
+                        </div>
+                        <div>
+                            <h3>5</h3>
+                        </div>
+                        <div>
+                            <h3>6</h3>
+                        </div>
+                        <div>
+                            <h3>7</h3>
+                        </div>
+                        <div>
+                            <h3>8</h3>
+                        </div>
+                    </Slider>
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default FreeBook
